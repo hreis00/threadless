9ede6840630db108991afaad59e0779dc38458b3
hreis00 <hbssreis@gmail.com>
2024-06-11 14:44:40 +0100

    useFollow hook added

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -46,7 +46,6 @@ export const followUnfollowUser = async (req, res) => {
       await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
       await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
 
-      // TODO: return the id of the user as a response
       res.status(200).json({ message: "Unfollowed user" });
     } else {
       // Follow user
@@ -60,7 +59,6 @@ export const followUnfollowUser = async (req, res) => {
       });
       await newNotification.save();
 
-      // TODO: return the id of the user as a response
       res.status(200).json({ message: "Followed user" });
     }
   } catch (error) {
--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
-import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
-
 import { useQuery } from "@tanstack/react-query";
+
+import useFollow from "../../hooks/useFollow";
+
+import RightPanelSkeleton from "../skeletons/RightPanelSkeleton";
+import LoadingSpinner from "./LoadingSpinner";
 
 const RightPanel = () => {
   const { data: suggestedUsers, isLoading } = useQuery({
@@ -19,6 +22,8 @@ const RightPanel = () => {
       }
     },
   });
+
+  const { follow, isPending } = useFollow();
 
   if (suggestedUsers?.length === 0) return <div className="w-0 md:w-64"></div>;
 
@@ -61,9 +66,12 @@ const RightPanel = () => {
                 <div>
                   <button
                     className="text-black bg-white rounded-full btn hover:bg-white hover:opacity-90 btn-sm"
-                    onClick={(e) => e.preventDefault()}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      follow(user._id);
+                    }}
                   >
-                    Follow
+                    {isPending ? <LoadingSpinner size="sm" /> : "Follow"}
                   </button>
                 </div>
               </Link>
